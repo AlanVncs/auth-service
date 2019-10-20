0a5f64ceb6f87a1c747d97e89b56a719a5a91229
Alan Vinicius Silva <alanvinicios@gmail.com>
2019-10-20 16:55:36 -0300

    jwt service

---
+++ src/services/jwt.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const jwtHash = process.env.JWT_HASH;
+
+console.log('jwthash: ' + jwtHash);
+
+
+module.exports = {
+    'getToken': (object) => {
+        if(jwtHash){
+            return jwt.sign(object, jwtHash, {});
+        }
+        else{
+            throw {error: "You must provide a JWT_HASH inside src/.env file"}
+        }
+    },
+    'verify': (token) => {
+        return jwt.verify(token, jwtHash, {});
+    }
+}
